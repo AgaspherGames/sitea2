cf4fe14db7b4d3d7f399e2ac56fc173e8a3981fc
Agaspher <100010896+AgaspherGames@users.noreply.github.com>
2023-01-28 13:25:15 +0600

    Update cookie.inc.php

--- inc/cookie.inc.php
+++ inc/cookie.inc.php
@@ -1,10 +1,14 @@
 <?php
 error_reporting(0);
-$visitCounter = $_COOKIE['visitCounter']?:0;
+$visitCounter = $_COOKIE['visitCounter'] ?: 0;
 $visitCounter++;
-$lastVisit =$_COOKIE['lastVisit']?date('Y-m-d H:i',$_COOKIE['lastVisit']):'';
-if (date('d-m-Y', $_COOKIE['lastVisit']) != date('d-m-Y')){
-  setcookie('visitCounter', $visitCounter, time()+3600*24);
-  setcookie('lastVisit', time(), time()+3600*24);
+$lastVisit = $_COOKIE['lastVisit'] ? date('Y-m-d H:i', $_COOKIE['lastVisit']) : '';
+if (date('d-m-Y', $_COOKIE['lastVisit']) != date('d-m-Y')) {
+  setcookie('visitCounter', $visitCounter, time() + 3600 * 24);
+  setcookie('lastVisit', time(), time() + 3600 * 24);
 }
 
+//Без условия работает показывает, количество посещений и последний раз
+
+// setcookie('visitCounter', $visitCounter, time() + 3600 * 24);
+// setcookie('lastVisit', time(), time() + 3600 * 24);
